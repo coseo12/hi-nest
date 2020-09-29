a732369cd61069309909fcf7d454aba57db0b0e5
coseo12 <momo_12@naver.com>
2020-09-29 14:31:50 +0900

    add

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Movie } from './entities/Movie.entitiy';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+  private movies: Movie[] = [];
+
+  getAll(): Movie[] {
+    return this.movies;
+  }
+
+  getOne(id: number): Movie {
+    const movie = this.movies.find(movie => movie.id === id);
+    if (!movie) throw new NotFoundException(`Movie with ID ${id} not found.`);
+    return movie;
+  }
+
+  deleteOne(id: number): void {
+    this.getOne(id);
+    this.movies = this.movies.filter(movie => movie.id !== id);
+  }
+
+  create(movieData: CreateMovieDto): void {
+    this.movies.push({
+      id: new Date().getTime(),
+      ...movieData,
+    });
+  }
+
+  update(id: number, updateData: UpdateMovieDto): void {
+    const movie = this.getOne(id);
+    this.deleteOne(id);
+    this.movies.push({ ...movie, ...updateData });
+  }
+}
